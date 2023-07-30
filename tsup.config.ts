6acc9ede713273138b70c3ae4becc4533d11333a
Ibe Dwi <hi@ibedwi.dev>
2023-07-30 16:56:47 +0700

    [fix] Next.js 'use client', tsup.config, and missing React (#6)
    * fix: tsup config
    
    * fix: missing react and tsup config
    
    * chore: bump patch version

---
+++ tsup.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  minify: true,
+  target: "es2018",
+  sourcemap: true,
+  external: ["react", "react-dom"],
+  // Generate declaration files (.d.ts)
+  dts: true,
+  format: ["esm", "cjs"],
+  // Add "use client" string for Next.js to detect client-side code
+  // 1. https://tsup.egoist.dev/#custom-esbuild-plugin-and-options
+  // 2. https://esbuild.github.io/api/#banner
+  esbuildOptions(options) {
+    options.banner = {
+      js: '"use client"',
+    };
+  },
+});
